e677d6a79eead21cd0a7dacb2f1a41878740af1c
Timileyin Odegbile <odegbiletimileyin11@gmail.com>
2022-12-18 21:58:35 +0100

    Testing the App with both Async-await and fetch

--- index.js
+++ index.js
@@ -1,66 +1,83 @@
-fetch("https://jsonplaceholder.typicode.com/").then((data) => {
-    return data.json(); 
-}).then((objectData) => {
-    let tableData="";
-    objectData.map((values) => {
-        tableData+=`<tr>
-        <td>${values.id}</td>
-        <td>${values.name}</td>
-        <td>${values.username}</td>
-        <td>${values.email}</td>
-        <td>${values.address.city}</td>
-      </tr>`
+// ============== ASYNC-AWAIT APPROACH ================
+
+//   .catch((err) => {
+//     if (err) {
+//       document.querySelector(".table").classList.add("hide");
+//       document.querySelector(".error-info").classList.remove("hide");
+//     }
+//   });
+
+
+    // ============== ANOTHER ONE =======================
+
+async function getData() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    console.log(res);
+    const data = res.json();
+    return data;
+}
+getData().then((myData) => {
+    let tableData = '';
+    myData.map((value) => {
+        tableData.innerHTML += `<tr>
+        <td>${value.id}</td>
+        <td>${value.name}</td>
+        <td>${value.email}</td>
+        <td>${value.website}</td>
+        </tr>`
     });
     document.getElementById('table-body').innerHTML=tableData;
+
 }).catch((err) => {
-    if (err) {
-        document.querySelector('.table').classList.add('hide');
-        document.querySelector('.error-info').classList.remove('hide');
-    }
+    console.log(err);
+});
 
-})
-
-
-
-
-
+    // .catch((err) => {
+        // if (err) {
+        //     document.querySelector(".table").classList.add("hide");
+        //     document.querySelector(".error-info").classList.remove("hide");
+        //     }
+    //      });
 
 
-// async function loadIntoTable(url) {
-//   const res = await fetch(url);
-//   const response = await res.json();
 
-//       console.log(response);
 
-//     //   const tableHead = table.querySelector('thead');
-//     //   const tableBody = table.querySelector('tbody');
-//     // const { header, row } = await response.json();
-
-//   // // Clear the table
-//   // tableHead.innerHTML = '<tr></tr>';
-//   // tableBody.innerHTML = '';
-
-//   // //Populate the headers
-//   // for (const headText of headers) {
-//   //     const headerElement = document.createElement('th');
-
-//   //     headerElement.textContent = headText;
-//   //     tableHead.querySelector('tr').appendChild(headerElement);
-//   // }
-
-//   // //Populate the row
-// //   for (const row of response.data) {
-// //       const rowElement = document.createElement('tr');
-
-// //       for (const cellText of row) {
-// //           const cellElement = document.createElement('td');
-
-// //           cellElement.textContent = cellText;
-// //           rowElement.appendChild(cellElement);
-// //       }
-// //       tableBody.appendChild(rowElement);
-// //   }
+//     response.map((value) => {
+//         return (
+//             tableData.innerHTML += `<tr>
+//                 <td>${value.id}</td>
+//                 <td>${value.name}</td>
+//                 <td>${value.email}</td>
+//                 <td>${value.website}</td>
+//             </tr>`
+//         )
+//     })
+    
 // }
+// getData();
 
-// loadIntoTable(
-//   "https://jsonplaceholder.typicode.com/users?postId=1");
+
+
+// ============== FETCH-THEN-CATCH APPROACH ================
+
+// const getData = fetch("https://jsonplaceholder.typicode.com/users").then((data) => {
+//     return data.json();
+//     console.log(data);
+//     }).then((objectData) => {
+//     let tableData="";
+//     objectData.map((values) => {
+//         tableData+=`<tr>
+//         <td>${values.id}</td>
+//         <td>${values.name}</td>
+//         <td>${values.email}</td>
+//         <td>${values.company.catchPhrase}</td>
+//       </tr>`
+//     });
+//     document.getElementById('table-body').innerHTML=tableData;
+//     }).catch((err) => {
+//     if (err) {
+//         document.querySelector('.table').classList.add('hide');
+//         document.querySelector('nav').classList.add('hide');
+//         document.querySelector('.error-info').classList.remove('hide');
+//     }
+// });
