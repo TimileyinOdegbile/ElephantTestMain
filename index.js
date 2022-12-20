13644e795623a74ea5a67959b4b71e18ffd39e72
Timileyin Odegbile <odegbiletimileyin11@gmail.com>
2022-12-21 00:08:29 +0100

    Some final touches

--- index.js
+++ index.js
@@ -1,36 +1,61 @@
-// ============== ASYNC-AWAIT APPROACH ================
 
-//   .catch((err) => {
-//     if (err) {
-//       document.querySelector(".table").classList.add("hide");
-//       document.querySelector(".error-info").classList.remove("hide");
-//     }
-//   });
+// ============== FETCH-THEN-CATCH APPROACH ================
 
-
-    // ============== ANOTHER ONE =======================
-
-async function getData() {
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    console.log(res);
-    const data = res.json();
-    return data;
-}
-getData().then((myData) => {
-    let tableData = '';
-    myData.map((value) => {
-        tableData.innerHTML += `<tr>
-        <td>${value.id}</td>
-        <td>${value.name}</td>
-        <td>${value.email}</td>
-        <td>${value.website}</td>
-        </tr>`
+const getData = fetch("https://jsonplaceholder.typicode.com/users").then((data) => {
+    return data.json();
+    console.log(data);
+    }).then((objectData) => {
+    let tableData="";
+    objectData.map((values) => {
+        tableData+=`<tr>
+        <td>${values.id}</td>
+        <td>${values.name}</td>
+        <td>${values.email}</td>
+        <td>${values.company.catchPhrase}</td>
+      </tr>`
     });
     document.getElementById('table-body').innerHTML=tableData;
-
-}).catch((err) => {
-    console.log(err);
+    }).catch((err) => {
+    if (err) {
+        document.querySelector('.table').classList.add('hide');
+        document.querySelector('nav').classList.add('hide');
+        document.querySelector('.error-info').classList.remove('hide');
+    }
 });
+
+
+// ============== -ASYNC-AWAIT = FETCH-THEN-CATCH APPROACH ================
+
+// async function getData() {
+//     const dataFetch = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data = dataFetch.json();
+//         console.log(data);
+//     }
+
+   // ============== ANOTHER ONE =======================
+
+// async function getData() {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/users").then((data) => {
+//         const objectData = res.json();
+//         return objectData;
+//     });
+//     console.log(res);
+// }
+// getData().then((myData) => {
+//     let tableData = '';
+//     myData.map((value) => {
+//         tableData.innerHTML += `<tr>
+//         <td>${value.id}</td>
+//         <td>${value.name}</td>
+//         <td>${value.email}</td>
+//         <td>${value.website}</td>
+//         </tr>`
+//     });
+//     document.getElementById('table-body').innerHTML = tableData;
+
+// }).catch((err) => {
+//     console.log(err);
+// });
 
     // .catch((err) => {
         // if (err) {
@@ -40,44 +65,3 @@ getData().then((myData) => {
     //      });
 
 
-
-
-//     response.map((value) => {
-//         return (
-//             tableData.innerHTML += `<tr>
-//                 <td>${value.id}</td>
-//                 <td>${value.name}</td>
-//                 <td>${value.email}</td>
-//                 <td>${value.website}</td>
-//             </tr>`
-//         )
-//     })
-    
-// }
-// getData();
-
-
-
-// ============== FETCH-THEN-CATCH APPROACH ================
-
-// const getData = fetch("https://jsonplaceholder.typicode.com/users").then((data) => {
-//     return data.json();
-//     console.log(data);
-//     }).then((objectData) => {
-//     let tableData="";
-//     objectData.map((values) => {
-//         tableData+=`<tr>
-//         <td>${values.id}</td>
-//         <td>${values.name}</td>
-//         <td>${values.email}</td>
-//         <td>${values.company.catchPhrase}</td>
-//       </tr>`
-//     });
-//     document.getElementById('table-body').innerHTML=tableData;
-//     }).catch((err) => {
-//     if (err) {
-//         document.querySelector('.table').classList.add('hide');
-//         document.querySelector('nav').classList.add('hide');
-//         document.querySelector('.error-info').classList.remove('hide');
-//     }
-// });
