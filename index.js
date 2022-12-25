ad42f8cee600acf9fff5768c3679087d0bedf1fa
Timileyin Odegbile <odegbiletimileyin11@gmail.com>
2022-12-25 23:24:10 +0100

    Added loading state and Assigning click to rows

--- index.js
+++ index.js
@@ -1,67 +1,87 @@
 
 // ============== FETCH-THEN-CATCH APPROACH ================
 
-const getData = fetch("https://jsonplaceholder.typicode.com/users").then((data) => {
-    return data.json();
-    console.log(data);
-    }).then((objectData) => {
-    let tableData="";
-    objectData.map((values) => {
-        tableData+=`<tr>
+// const getData = fetch("https://jsonplaceholder.typicode.com/users").then((data) => {
+//     return data.json();
+//     }).then((objectData) => {
+//     let tableData="";
+//     objectData.map((values) => {
+    //     tableData+=`<tr>
+    //     <td>${values.id}</td>
+    //     <td>${values.name}</td>
+    //     <td>${values.email}</td>
+    //     <td>${values.company.catchPhrase}</td>
+    //   </tr>`
+//     });
+    // document.getElementById('table-body').innerHTML=tableData;
+//     }).catch((err) => {
+    // if (err) {
+    //     document.querySelector('.table').classList.add('hide');
+    //     document.querySelector('nav').classList.add('hide');
+    //     document.querySelector('.error-info').classList.remove('hide');
+    // }
+// });
+
+
+
+// ============== -ASYNC-AWAIT = FETCH-THEN-CATCH APPROACH ================
+
+async function getData() {
+    const dataFetch = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await dataFetch.json();
+        return data;
+}
+
+window.addEventListener('load', () => {
+    const loader = document.querySelector(".loader");
+
+    loader.classList.add("loader-hidden");
+
+    loader.addEventListener("transitionend", () => {
+        const field = document.querySelectorAll("field-box");
+        field.removeChild(".loader")
+    } )
+})
+
+getData().then((data) => {
+    let tableData = '';
+    data.map((values) => {
+        tableData+=`<tr data-href="https://jsonplaceholder.typicode.com/users">
         <td>${values.id}</td>
         <td>${values.name}</td>
         <td>${values.email}</td>
         <td>${values.company.catchPhrase}</td>
       </tr>`
-    });
-    document.getElementById('table-body').innerHTML=tableData;
-    }).catch((err) => {
+    })
+    document.getElementById('table-body').innerHTML = tableData;
+}).catch((err) => {
     if (err) {
         document.querySelector('.table').classList.add('hide');
         document.querySelector('nav').classList.add('hide');
         document.querySelector('.error-info').classList.remove('hide');
     }
+})
+
+$('.table').on("click", "tbody tr", function () {
+    
+    document.querySelector('.field-box').classList.add('hide');
+    document.querySelector('.information').classList.remove('hide');
+    
 });
 
 
-// ============== -ASYNC-AWAIT = FETCH-THEN-CATCH APPROACH ================
-
-// async function getData() {
-//     const dataFetch = await fetch("https://jsonplaceholder.typicode.com/users");
-//     const data = dataFetch.json();
-//         console.log(data);
-//     }
-
-   // ============== ANOTHER ONE =======================
-
-// async function getData() {
-//     const res = await fetch("https://jsonplaceholder.typicode.com/users").then((data) => {
-//         const objectData = res.json();
-//         return objectData;
-//     });
-//     console.log(res);
-// }
-// getData().then((myData) => {
-//     let tableData = '';
-//     myData.map((value) => {
-//         tableData.innerHTML += `<tr>
-//         <td>${value.id}</td>
-//         <td>${value.name}</td>
-//         <td>${value.email}</td>
-//         <td>${value.website}</td>
-//         </tr>`
-//     });
-//     document.getElementById('table-body').innerHTML = tableData;
-
-// }).catch((err) => {
-//     console.log(err);
-// });
-
-    // .catch((err) => {
-        // if (err) {
-        //     document.querySelector(".table").classList.add("hide");
-        //     document.querySelector(".error-info").classList.remove("hide");
-        //     }
-    //      });
 
 
+
+// getData((dataInfo) => {
+//     let personalInfo = '';
+//     dataInfo.map((info) => {
+//         personalInfo = `
+//         <h2>${info.name}</h2>
+//         <p>${info.email}</p>
+//         <h4>${info.address}</h4>
+//         <h4>${info.catchPhrase}</h4>
+//         `
+//         document.getElementsByClassName('.information').innerHTML = personalInfo;
+//     })
+// })
